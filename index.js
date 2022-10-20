3d2bfc922aec71eb56adcbb86d7625101badc749
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-20 19:03:49 +0530

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const router = require("./router/blog");
 
-const app = express()
+const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send('Hello from server')
-})
+app.use(express.static(path.join(__dirname, "static")));
+app.use("/", require(path.join(__dirname, "router / blog.js")));
 
 app.listen(port, () => {
-    console.log(`server running on port: ${port}📡`)
-})
+  console.log(`Server running on http://localhost:${port}/`);
+});
